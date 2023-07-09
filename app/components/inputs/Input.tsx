4b0b1be6ac4bdbe3de8e4878488c2178ee5f5abf
Mário Ponte <ponteolavo30@gmail.com>
2023-07-10 00:48:54 +0100

    Listing creation step 5 (Description and Price, Listing creation POST route)

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,6 +1,10 @@
 'use client';
 
-import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import {
+    FieldErrors,
+    FieldValues,
+    UseFormRegister
+} from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
 
 interface InputProps {
@@ -22,7 +26,7 @@ const Input: React.FC<InputProps> = ({
     formatPrice,
     register,
     required,
-    errors
+    errors,
 }) => {
     return (
         <div className="w-full relative">
@@ -37,7 +41,7 @@ const Input: React.FC<InputProps> = ({
                     "
                 />
             )}
-            <input 
+            <input
                 id={id}
                 disabled={disabled}
                 {...register(id, { required })}
@@ -47,9 +51,9 @@ const Input: React.FC<InputProps> = ({
                     peer
                     w-full
                     p-4
-                    pt-6
-                    font-light
-                    bg-white
+                    pt-6 
+                    font-light 
+                    bg-white 
                     border-2
                     rounded-md
                     outline-none
@@ -57,32 +61,32 @@ const Input: React.FC<InputProps> = ({
                     disabled:opacity-70
                     disabled:cursor-not-allowed
                     ${formatPrice ? 'pl-9' : 'pl-4'}
-                    ${errors[id] ? "border-rose-500" : "border-neutral-300"}
-                    ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
+                    ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
+                    ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
                 `}
             />
             <label
                 className={`
-                    absolute
+                    absolute 
                     text-md
-                    duration-150
-                    transform
-                    -translate-y-3
-                    top-5
-                    z-10
-                    origin-[0]
-                    ${formatPrice ? "left-9" : "left-4"}
-                    peer-placeholder-shown:scale-100
-                    peer-placeholder-shown:translate-y-0
+                    duration-150 
+                    transform 
+                    -translate-y-3 
+                    top-5 
+                    z-10 
+                    origin-[0] 
+                    ${formatPrice ? 'left-9' : 'left-4'}
+                    peer-placeholder-shown:scale-100 
+                    peer-placeholder-shown:translate-y-0 
                     peer-focus:scale-75
                     peer-focus:-translate-y-4
-                    ${errors[id] ? "border-rose-500" : "border-zinc-400"}
+                    ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
                 `}
             >
                 {label}
             </label>
         </div>
-    )
+    );
 }
 
 export default Input;
