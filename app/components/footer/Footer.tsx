9eaeeeb7f83b271671fbc2868da912e8aac17d0f
Mário Ponte <ponteolavo30@gmail.com>
2023-07-21 21:37:58 +0100

    Creating icons element

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,13 +2,14 @@
 
 import Image from "next/image";
 import Container from "../Container";
+import { LogoMario } from "@/app/icons";
 
 const Footer = () => {
     return (
         <div className="bg-white border-t-[1px] sticky top-[100vh]">
             <Container>
                 <div className="flex flex-auto items-center text-sm">
-                    <Image src="/images/logoMario.svg" className="mr-8" width={50} height={50} alt="logo" />
+                    <LogoMario className="h-16"/>
                     © All Rights Reserved to Mário Ponte - 2023
                 </div>
             </Container>
