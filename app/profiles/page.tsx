990b73d36da46bf6d8f981f6fd84a34c3efd40a4
Mário Ponte <ponteolavo30@gmail.com>
2023-11-22 18:05:32 +0000

    starting the creation of profile page styles

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,11 +1,14 @@
 import EmptyState from "@/app/components/EmptyState";
 import ClientOnly from "@/app/components/ClientOnly";
 import getCurrentUser from "@/app/actions/getCurrentUser";
+import Container from "../components/Container";
+import Image from "next/image";
+import { AiFillStar, AiFillHeart, AiFillHome } from "react-icons/ai";
 
 const ProfilePage = async () => {
     const currentUser = await getCurrentUser();
 
-    if(!currentUser){
+    if (!currentUser) {
         return (
             <ClientOnly>
                 <EmptyState
@@ -18,9 +21,41 @@ const ProfilePage = async () => {
 
     return (
         <ClientOnly>
-            <div>
-                Profile not available now
-            </div>
+            <Container>
+                <div className="flex flex-row gap-10">
+                    <div className="border-[1px] border-neutral-200 p-10 rounded-xl">
+                        <div>
+                            <Image
+                                className="rounded-full"
+                                height="200"
+                                width="200"
+                                alt="Avatar"
+                                src={"/images/placeholder.jpg"}
+                            />
+                        </div>
+                        <hr className="mt-4 mb-4" />
+                        <div className=" space-y-4">
+                            <div className="flex flex-row items-center content-center text-lg space-x-3">
+                                <AiFillStar size={20} color="#14b8a6" /><span>5 reviews</span>
+                            </div>
+                            <div className="flex flex-row items-center content-center text-lg space-x-3">
+                                <AiFillHeart size={20} color="#14b8a6" /><span>3 favorites</span>
+                            </div>
+                            <div className="flex flex-row items-center content-center text-lg space-x-3">
+                                <AiFillHome size={20} color="#14b8a6" /><span>6 properties</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="space-y-4">
+                        <div className="text-4xl font-semibold">
+                            Hi, I'm Manuel Silva
+                        </div>
+                        <div>
+                            Joined in 2014
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </ClientOnly>
     );
 }
