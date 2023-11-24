44d9a2ce11e3ceaceef957c181b30b674dd9801b
Mário Ponte <ponteolavo30@gmail.com>
2023-11-24 14:38:07 +0000

    finishing styles for profile pages

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -4,8 +4,16 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import Container from "../components/Container";
 import Image from "next/image";
 import { AiFillStar, AiFillHeart, AiFillHome } from "react-icons/ai";
+import Heading from "../components/Heading";
+import getListings, { IListingsParams } from "../actions/getListings";
+import ListingCard from "../components/listings/ListingCard";
 
-const ProfilePage = async () => {
+interface HomeProps {
+    searchParams: IListingsParams
+};
+
+const ProfilePage = async ({ searchParams }: HomeProps) => {
+    const listings = await getListings(searchParams);
     const currentUser = await getCurrentUser();
 
     if (!currentUser) {
@@ -53,6 +61,33 @@ const ProfilePage = async () => {
                         <div>
                             Joined in 2014
                         </div>
+                    </div>
+                </div>
+                <div className="mt-10">
+                    <Heading
+                        title="Properties"
+                        subtitle="List of properties owned by Manuel Silva"
+                    />
+                    <div className="
+                        mt-10
+                        grid 
+                        grid-cols-1 
+                        sm:grid-cols-2 
+                        md:grid-cols-3 
+                        lg:grid-cols-4
+                        xl:grid-cols-5
+                        2xl:grid-cols-6
+                        gap-8
+                    ">
+                        {listings.map((listing) => {
+                            return (
+                                <ListingCard
+                                    currentUser={currentUser}
+                                    key={listing.id}
+                                    data={listing}
+                                />
+                            )
+                        })}
                     </div>
                 </div>
             </Container>
